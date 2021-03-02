5db5c2744f8de110ce0c5df66ed1e7ed2d01aa80
vlainvaldez <alvinjosephvaldez@gmail.com>
2021-03-02 23:41:48 +0800

    first commit

---
+++ MyWeatherApp/Forecast.swift
@@ -0,0 +1,8 @@
+//
+//  Forecast.swift
+//  MyWeatherApp
+//
+//  Created by alvin joseph valdez on 3/2/21.
+//
+
+import Foundation
---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by alvin joseph valdez on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
